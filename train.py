85d39f354eed0ba4419b641b4f4b24abb7b2ff16
mahad852 <mahad852@gmail.com>
2026-02-15 21:25:08 -0500

    using lower pos_weight

--- train.py
+++ train.py
@@ -137,7 +137,7 @@ def main():
     model = get_model(args).to(device)
 
     optim = Optim.Adam(params=model.parameters(), lr=1e-4)
-    criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([35], device=device))
+    criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([15], device=device))
 
     negs = 0.0
     poss = 0.0
